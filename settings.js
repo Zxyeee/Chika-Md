833d5e885dbf11fcfbcc20c89a2f1a4e2826ee97
Zxyeee <134941902+Zxyeee@users.noreply.github.com>
2023-05-29 20:16:34 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6285236669891','6285236669891','6285236669891'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6285236669891'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6285236669891' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Ryan' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Ryan' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Ryan' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
